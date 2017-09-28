18337d763116ccb46bd5fb74772ea8518ddee0e4
Megan Nguyen <nguyenmegan@berkeley.edu>
2017-09-27 22:49:07 -0700

    done!

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,11 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+      @baz = baz
   end
+
+  def bar(sym, hash)
+      return sym.to_s + @baz.to_s + hash[:sat].to_s
+  end
+
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -8,17 +8,17 @@ class Person
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    return @name.slice(0,4)
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    return (2017 - @age.to_i).to_s
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    return @name.to_s + @age.to_s
   end
 
 end
